18bfc5a0e98a54792de39eba38fc9abe862c5e73
JD Porterfield <jdvporterfield@gmail.com>
2015-09-19 20:09:51 -0500

    README and code cleanup
    I cleaned up a lot of the code, added comments and favicons, and also a
    readme.

---
+++ README.txt
@@ -0,0 +1,9 @@
+FLAPPY BIRD
+
+This is a project that I made for a web development class.
+
+I do not take credit for the original idea of the game and this is basically a clone of Flappy Bird by Dong Nguyen. However, I did create most of the sprites for this version, except for the 'Ready' graphic which I edited from a Flappy Bird graphic. 
+
+The reason that I used a bat instead of a bird was because I believed it would make more sense for a bat in a cave to dodge obstacles (stalactites and stalagmites) than for a bird to be dodging pipes that seemingly come from nowhere (at least the top ones). Plus, I thought it would be a fun little challenge to see if I could draw a cave and a bat successfully.
+
+As my first project writing the complete css, javascript, and html for the entire thing, I am pretty proud. However, there are things that I hope to get better at. For instance, if I had more time, it might be cool to do the project with Canvas rather than just manipultaing divs on the page (it might also be more efficient, but I'm not sure yet).
--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 /* Author : JD Porterfield */
-
-var bat;
-var dimension;
-var y;
 var batImg = ["url(\"img/bat1.png\")","url(\"img/bat3.png\")","url(\"img/bat2.png\")"];
 var resSizes = {"batWidth" : 177, "batHeight" : 134, "backgroundWidth" : 1080, "backgroundHeight" : 1920, "readyWidth" : 540, "readyHeight" : 550, "pipeWidth" : 180};
 var difficulty;
+var dimension;
 var movement;
 
-var batHeight;
 var pipes = [];
 var pipeCounter;
 var score;
@@ -18,8 +14,13 @@ var endCard;
 var myInterval;
 var pipeInterval;
 var stepsLeft;
+var bat;
+var y;
 
-
+/*
+This is basically the initialization function.
+It determines the placement of all of the components based on the size of the view.
+*/
 function determineSizes(){
 	
 	dimension = {};
@@ -46,7 +47,7 @@ function determineSizes(){
 	
 	//bat
 	bat = document.getElementById("bat");
-	batHeight = (h * (resSizes["batHeight"]/resSizes["backgroundHeight"]));
+	var batHeight = (h * (resSizes["batHeight"]/resSizes["backgroundHeight"]));
 	bat.style.height =  batHeight + "px";
 	var batWidth = (width * (resSizes["batWidth"]/resSizes["backgroundWidth"]));
 	dimension["batWidth"] = batWidth;
@@ -69,12 +70,12 @@ function determineSizes(){
 	dimension["pipemovespeed"] = (width * 12 / resSizes["backgroundWidth"]);
 	difficulty["pipeGap"] = (h * 600 / resSizes["backgroundHeight"]);
 	difficulty["pipeVerticalDistance"] = (h * 400 / resSizes["backgroundHeight"]);
-	difficulty["minPipeGap"] = (h * (resSizes["batHeight"] + 50) / resSizes["backgroundHeight"]);
+	difficulty["minPipeGap"] = (h * (resSizes["batHeight"] * 2) / resSizes["backgroundHeight"]);
 	difficulty["pipeGapRate"] = (h * 25 / resSizes["backgroundHeight"]);
 	dimension["pipeCenter"] = (h / 2);
 	
 	//scale the jump speed and acceleration accordingly for the size of the canvas
-	movement["jumpSpeed"] = (h * (-30 / resSizes["backgroundHeight"]));
+	movement["jumpSpeed"] = (h * (-28 / resSizes["backgroundHeight"]));
 	movement["fallAcceleration"] = (h * (2 / resSizes["backgroundHeight"]));
 	
 	//scale the end card
@@ -95,16 +96,36 @@ function determineSizes(){
 	}
 }
 
+/*
+Increases the difficulty of the game.
+*/
 function increaseDifficulty(){
 	if(difficulty["pipeGap"] > difficulty["minPipeGap"]){
 		difficulty["pipeGap"] -= difficulty["pipeGapRate"];
 	}
 	
-	if(difficulty["pipeDistance"] > 10){
-		difficulty["pipeDistance"] -= 1;
+	if(difficulty["pipeDistance"] > 36){
+		difficulty["pipeDistance"] -= 4;
+	}
+	
+	if(difficulty["pipeVerticalDistance"] < dimensions["height"]){
+		difficulty["pipeVerticalDistance"] -= difficulty["pipeGapRate"];
 	}
 }
 
+/*
+If the user wants an immediate increase of difficulty, this method is used (to checkt )
+*/
+function manualDifficultyIncrease(e){
+	if(e.which == 13){
+		increaseDifficulty();
+	}
+}
+
+/*
+Called to create an animation of the bat flapping its wings.
+Flapping its wings obviously creates lift, so this is also applied.
+*/
 function flapWings(){
 	bat.style.backgroundImage = batImg[0];
 	setTimeout(function(){
@@ -117,6 +138,10 @@ function flapWings(){
 	fall();
 }
 
+/*
+This function is called when the player collides with an obstacle.
+Displays the score and high score and gets the game ready to be played again.
+*/
 function loseGame(){
 	clearInterval(myInterval);
 	clearInterval(pipeInterval);
@@ -141,35 +166,40 @@ function loseGame(){
 	setTimeout(function(){document.onclick = restart}, 1000);
 }
 
+/*
+Causes the bat to fall based on its vertical speed. Also applies acceleration downward due to gravity. 
+*/
 function fall(){
 	movement["verticalSpeed"] += movement["fallAcceleration"];
 	y += movement["verticalSpeed"];
 	bat.style.top = y + "px";
 	
-	if( y < 0 || y + batHeight > dimension["viewHeight"]){
+	if( y < 0 || y + dimension["batHeight"] > dimension["viewHeight"]){
 		loseGame();
 	}
 	checkCollisions();
 }
 
+/*
+Checks whether the bat has collided with the floor, ceiling, or any obstacles.
+*/
 function checkCollisions(){
-	
 	//floor and ceiling
-	if( y < 0 || y + batHeight > dimension["viewHeight"]){
+	if( y < 0 || y + dimension["batHeight"] > dimension["viewHeight"]){
 		loseGame();
 	}
-	
 	//the pipe obstacles
 	for(var i = 0; i < pipes.length; i++){		
-		 
 		if(checkDivCollisionWithBat(i)){
 			loseGame();
 		}
 	}
 }
 
+/*
+Checks whether or not the bat has collided with the given div element.
+*/
 function checkDivCollisionWithBat(div){
-	
 	//I create elements a, b, and c so that the checking expressions will be easier to understand
 	var a = {"left" : dimension["viewWidth"]/2 - dimension["batWidth"]/2 , "top" : y, "bottom" : y + dimension["batHeight"]};
 	a["right"] = a["left"] + dimension["batWidth"];
@@ -180,37 +210,38 @@ function checkDivCollisionWithBat(div){
 	return checkGenericCollision(a, b) || checkGenericCollision(a, c);
 }
 
+/*
+Checks whether two div elements have collided.
+I abstracted this out in case I later wanted to add other obstacles.
+*/
 function checkGenericCollision(a, b){
-	
 	//right corners
 	if(a.right > b.left && a.right < b.right){
-		
 		//top right
 		if(a.top < b.bottom && a.top > b.top){
 			return true;
 		}
-		
 		//bottom right
 		if(a.bottom > b.top && a.bottom < b.bottom){
 			return true;
 		}
 	}
-	
 	//left corners
 	if(a.left > b.left && a.left < b.right){
-		
 		if(a.top > b.top && a.top < b.bottom){
 			return true;
 		}
-		
 		if(a.bottom > b.top && a.bottom < b.bottom){
 			return true;
 		}
 	}
-	
 	return false;
 }
 
+/*
+Figures out the placement of the next pipe. 
+This is an important step because it is important to not have the pipes too far apart (or it will be impossible) or together (it won't be fun).
+*/
 function changePipePosition(){
 	
 	var neg = Math.random() > .5;
@@ -231,9 +262,14 @@ function changePipePosition(){
 	}
 }
 
+/*
+Moves every pipe div element to the left in order to make it appear that the obstacles are coming for the bat.
+*/
 function movePipes(){
 	
+	//create a new pipe
 	if(stepsLeft == 0){
+		
 		var back = document.getElementById("pipeContainer");
 		var left = "100%";
 		var name = "pipe" + pipeCounter;
@@ -243,15 +279,15 @@ function movePipes(){
 		back.innerHTML = back.innerHTML + "	<div id=\""+name+"top\" class=\"pipe\"></div><div id=\""+name+"bottom\" class=\"pipe\"></div>";
 		var pipe = document.getElementById(name+"top");
 		
-		
+		//set the top's positioning
 		pipe.style.width = dimension["pipeWidth"] + "px";
 		thisPipe["height"] = dimension["pipeCenter"] - (difficulty["pipeGap"] / 2) 
 		pipe.style.height = thisPipe["height"] + "px";
 		pipe.style.left = left + "px";
 		pipe.style.top = "0%";
 		
+		//set the bottom's positioning
 		pipe = document.getElementById(name+"bottom");
-		
 		pipe.style.width = dimension["pipeWidth"] + "px";
 		pipe.style.height = dimension["viewHeight"] - (dimension["pipeCenter"] + (difficulty["pipeGap"] / 2)) + "px";
 		pipe.style.left = left + "px";
@@ -270,13 +306,14 @@ function movePipes(){
 		t.style.left = pipes[i]["left"] + "px";
 		b.style.left = pipes[i]["left"] + "px";
 		
-		
+		//if the pipe is offscreen, remove it
 		if(pipes[i]["left"] < (0 - dimension["pipeWidth"] )){
 			pipes.splice(i, 1);
 			t.parentNode.removeChild(t);
 			b.parentNode.removeChild(b);
 		}
 		
+		//if the bat has passed a certain pipe, increase the score (only once)
 		else if((pipes[i]["left"] + dimension["pipeWidth"] < (dimension["viewWidth"]/2) - (dimension["batWidth"]/2))  && pipes[i]["passed"] == false){
 			pipes[i]["passed"] = true;
 			score += 1;
@@ -289,6 +326,9 @@ function movePipes(){
 	stepsLeft -= 1;
 }
 
+/*
+Starts the game by showing/hiding the correct elements.
+*/
 function start(){
 	document.getElementById("startGraphic").style.visibility = "hidden";
 	scoreDisplay = document.getElementById("score");
@@ -300,6 +340,9 @@ function start(){
 	document.onclick = flapWings;
 }
 
+/*
+Shows the start graphic again so that the user can have time to get ready for a new game.
+*/
 function restart(){
 	document.getElementById("startGraphic").style.visibility = "initial";
 	bat.style.visibility = "initial";
@@ -307,9 +350,13 @@ function restart(){
 	document.onclick = start;
 }
 
+/*
+Once all of the HTML has loaded, calls for all of the elements to be resized.
+*/
 document.addEventListener('DOMContentLoaded', function () {
 	determineSizes();
 	document.onclick = start;
+	document.onkeypress = manualDifficultyIncrease;
 	document.ondragstart = function() { return false; };
 	document.onselectstart = function(){ return false; };
 });
